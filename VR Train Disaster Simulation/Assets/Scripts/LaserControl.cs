ccd92f85ad4eb2d150985d43dad67d72377d7f20
prasetion <prasetio.nugroho@octagonstudio.com>
2019-05-25 20:33:57 +0700

    fix raycast system

--- VR Train Disaster Simulation/Assets/Scripts/LaserControl.cs
+++ VR Train Disaster Simulation/Assets/Scripts/LaserControl.cs
@@ -4,16 +4,33 @@ using UnityEngine;
 
 public class LaserControl : MonoBehaviour {
 
-    public GameObject LeftLaser;
-    public GameObject RightLaser;
+    private LineRenderer _Laser;
 
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    void Start()
+    {
+        _Laser = GetComponent<LineRenderer>();    
+    }
+
 	// Update is called once per frame
 	void Update () {
-		
+		LaserPosition();
+        MyRaycast();
 	}
+
+    void LaserPosition(){
+        _Laser.SetPosition(0, transform.position);
+        _Laser.SetPosition(1, transform.position + transform.forward * 100);
+    }
+
+    void MyRaycast(){
+        RaycastHit hit;
+        Ray myRay = new Ray(transform.position, transform.forward * 5000);
+
+        Debug.DrawRay(transform.position, transform.forward * 5000, Color.yellow);
+    }
+
 }
