4ff74b7b50115b86267554e3309a11c47d19de23
prasetion <prasetio.nugroho@octagonstudio.com>
2019-05-30 01:31:39 +0700

    update apar

---
+++ VR Train Disaster Simulation/Assets/Scripts/ObjectBehaviour/ChairBehaviour.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChairBehaviour : MonoBehaviour
+{
+   
+    void OnTriggerEnter(Collider other){
+        ScenarioManager.Instance.CounterStep += 1;     
+    }
+
+}
