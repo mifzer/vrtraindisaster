d8c809122cba4b051418b14f70979ae66ee2102f
prasetion <prasetio.nugroho@octagonstudio.com>
2019-06-23 12:11:00 +0700

    update simulasi palu
    kaca pecah setelah dipukul 5 kali

--- VR Train Disaster Simulation/Assets/Scripts/ObjectBehaviour/WindowBehaviour.cs
+++ VR Train Disaster Simulation/Assets/Scripts/ObjectBehaviour/WindowBehaviour.cs
@@ -6,24 +6,55 @@ public class WindowBehaviour : MonoBehaviour
 {
     
     private bool _IsTriggered = false;
+    private int _KnockCounter = 0;
 
+    [Header("Audio")]
     [SerializeField] private AudioSource _CrackAudio;
+    [SerializeField] private AudioSource _KnockAudio;
 
-    void OnTriggerEnter(Collider other){
+
+    // void OnTriggerEnter(Collider other){
         
-        if(other.CompareTag("Hammer")){
+    //     if(other.CompareTag("Hammer")){
+            
+    //         // if(_IsTriggered == true)
+    //         //     return;
+
+    //         Debug.Log("crack");
+    //         _CrackAudio.Play();
+
+    //         // show pop up
+    //         UIManager.Instance.ShowPopUp("kaca nya pecah");
+            
+    //         // balik ke main menu scene
+    //         ScenarioManager.Instance.FinishScenario();
+    //     }
+    // }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if(other.collider.CompareTag("Hammer")){
             
             // if(_IsTriggered == true)
             //     return;
+            _KnockCounter += 1;
 
-            Debug.Log("crack");
-            _CrackAudio.Play();
+            if(_KnockCounter == 5){
+                
+                _CrackAudio.Play();
 
-            // show pop up
-            UIManager.Instance.ShowPopUp("kaca nya pecah");
-            
-            // balik ke main menu scene
-            ScenarioManager.Instance.FinishScenario();
+                // show pop up
+                UIManager.Instance.ShowPopUp("kaca nya pecah");
+                
+                // balik ke main menu scene
+                ScenarioManager.Instance.FinishScenario();
+                return;
+            }
+
+            _KnockAudio.Play();
+            // Debug.Log("crack");
+            // _CrackAudio.Play();
+
         }
     }
 
